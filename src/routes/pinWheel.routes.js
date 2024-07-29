eca8ab99e335f1cbdb940057bd27421e451ba84c
Muhammad Hamza Liaqat <mh408800@gmail.com>
2024-07-29 14:34:36 +0500

    endPoint updated

--- src/routes/pinWheel.routes.js
+++ src/routes/pinWheel.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { verificationPinWheelIncome, createUser } = require("../controller/pinWheel.controller");
 const pinWheelRoutes = express.Router();
 
-pinWheelRoutes.post("/verification", verificationPinWheelIncome);
+pinWheelRoutes.get("/verification", verificationPinWheelIncome);
 pinWheelRoutes.post("/create/user", createUser);
 
 module.exports = pinWheelRoutes
