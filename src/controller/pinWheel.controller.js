0b4368b50ba72e1f50eb04e393b41f260d8a35c8
Muhammad Hamza Liaqat <mh408800@gmail.com>
2024-07-29 14:54:44 +0500

    getLinkToken added

--- src/controller/pinWheel.controller.js
+++ src/controller/pinWheel.controller.js
@@ -54,7 +54,37 @@ const createUser = async (req, res) => {
     }
 };
 
+const getLinkToken = async (req, res) => {
+    try {
+        const { userId } = req.body;
+
+        const response = await axios.post(
+            `${PINWHEEL_API_BASE_URL}/link_tokens`,
+            { user_id: userId },
+            {
+                headers: {
+                    'Authorization': `Bearer ${PINWHEEL_API_KEY}`,
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+
+        const linkToken = response.data;
+        return res.status(200).json({ message: "Link token generated successfully", data: linkToken });
+    } catch (error) {
+        console.error("An error occurred while generating the link token:", error);
+        // if (error.response) {
+        //     console.error("Error Response Data:", error.response.data);
+        //     console.error("Error Response Status:", error.response.status);
+        //     console.error("Error Response Headers:", error.response.headers);
+        //     return res.status(500).json({ message: "Internal server error", data: error.response.data });
+        // }
+        return res.status(500).json({ message: "Internal server error", data: error.message });
+    }
+};
+
 module.exports = {
     verificationPinWheelIncome,
-    createUser
+    createUser,
+    getLinkToken
 };
--- src/routes/pinWheel.routes.js
+++ src/routes/pinWheel.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { verificationPinWheelIncome, createUser } = require("../controller/pinWheel.controller");
+const { verificationPinWheelIncome, createUser, getLinkToken } = require("../controller/pinWheel.controller");
 const pinWheelRoutes = express.Router();
 
 pinWheelRoutes.get("/verification", verificationPinWheelIncome);
 pinWheelRoutes.post("/create/user", createUser);
+pinWheelRoutes.post("/link", getLinkToken);
 
 module.exports = pinWheelRoutes
